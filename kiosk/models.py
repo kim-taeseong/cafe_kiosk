a09f7b3ef749aae407b4a8eca1efc0c27ce7fd46
taeseong <dnfrkwhr2424@naver.com>
2024-04-16 17:45:28 +0900

    feat: 모델 Category, Item 생성

--- kiosk/models.py
+++ kiosk/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# 카테고리
+class Category(models.Model):
+    name = models.CharField(max_length=10)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+# 상세 메뉴
+class Item(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
