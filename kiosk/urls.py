9833e1d24fc2d2466f82ac637c627a6bb9fbcbea
taeseong <dnfrkwhr2424@naver.com>
2024-04-18 14:04:42 +0900

    feat(url): app name 추가

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'kiosk'
+
 urlpatterns = [
     path('', views.MenuListView.as_view(), name='index'),
     path('item/<int:pk>/', views.MenuDetailView.as_view(), name='detail'),
